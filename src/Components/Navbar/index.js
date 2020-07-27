83a5fc8c0c43cf520f67f34c82bb9f41eda1c1cd
jdeleonardis <jdeleonardis@hotmail.com>
2020-07-26 22:45:32 -0400

    changed the header to point to /myportfolio

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -6,11 +6,11 @@ import "../../Styles/style.css";
 function Navbar() {
   const location = useLocation();    
 
-  console.log(location.pathname)
+  //console.log(location.pathname)
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark border-bottom border-dark fixed-top">    
-        <a className="navbar-brand" id="navbar_name" href="/">James L DeLeonardis</a>
+        <a className="navbar-brand" id="navbar_name" href="/myportfolio">James L DeLeonardis</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
         </button>
