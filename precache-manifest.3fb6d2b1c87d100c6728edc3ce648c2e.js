481934a4a4a6ef0c8f7357b0fd8037c5e9510505
jdeleonardis <jdeleonardis@hotmail.com>
2020-07-26 22:31:35 -0400

    Updates

---
+++ precache-manifest.3fb6d2b1c87d100c6728edc3ce648c2e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f8c860ff73231fa040f488af8e8774",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "c4e264ed8280533dc816",
+    "url": "/home/static/css/main.de084e4f.chunk.css"
+  },
+  {
+    "revision": "c3a6458172a8c7d7ad6b",
+    "url": "/home/static/js/2.bfea3562.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/home/static/js/2.bfea3562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4e264ed8280533dc816",
+    "url": "/home/static/js/main.0e9b8e55.chunk.js"
+  },
+  {
+    "revision": "a1fec93b9144bf3582e0",
+    "url": "/home/static/js/runtime-main.87f3545d.js"
+  },
+  {
+    "revision": "6cc5578d01890f1504647b10ec3d78ee",
+    "url": "/home/static/media/Day Planner.6cc5578d.png"
+  },
+  {
+    "revision": "de8fec871d570413897f41629a0a4d66",
+    "url": "/home/static/media/Eat Da Burger.de8fec87.png"
+  },
+  {
+    "revision": "5aefba0bff06e25ee16dd9a173c3b4c3",
+    "url": "/home/static/media/Team Generator.5aefba0b.png"
+  },
+  {
+    "revision": "480e08d5f1ef6ec6c4fd8c12f593dcd4",
+    "url": "/home/static/media/Weather Dashboard.480e08d5.png"
+  },
+  {
+    "revision": "17b47ec93f2afbf54c50f6eec2ea11b6",
+    "url": "/home/static/media/pantrybuddy.17b47ec9.png"
+  },
+  {
+    "revision": "c0eb0fc213df8c66c9d6b7732085e78c",
+    "url": "/home/static/media/photo0.c0eb0fc2.jpg"
+  },
+  {
+    "revision": "7607013a03bce9ddfd9268f01bc172a5",
+    "url": "/home/static/media/smsol.7607013a.png"
+  }
+]);
